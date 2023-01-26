4b20858da47de94e5eff5f71c3cd5e5d892f48c7
Supakit Wiwustrinon <divertzt@gmail.com>
2023-01-26 16:28:44 +0700

    some stuff

---
+++ pages/db.sql
@@ -0,0 +1,11 @@
+INSERT INTO `Grades`(
+    `studentId`,
+    `Std_name`,
+    `Course_name`,
+    `score`
+)
+VALUES(
+    '6530250018', 'นายกฤษณพัฒน์ ประสมสุข', 'Fundamental Programming Concepts', 'C' 
+),(
+    '6530250026',	'นายกษิดิศ ทองเอมเอี่ยม',	'Fundamental Programming Concepts'	,'A'
+)
