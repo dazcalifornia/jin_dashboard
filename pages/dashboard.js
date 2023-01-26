5ffab83d2a53414dde391a394b72f444d53d4666
dazcalifornia <divertzt@gmail.com>
2023-01-26 09:11:42 +0700

    Tailwindcss update

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,40 +1,50 @@
+import { useRouter } from 'next/router';
 import { useState,useEffect } from 'react'
 
 export default function Dashboard(){
   const [data,setData] = useState([])
+
+  const router = useRouter()
   useEffect(()=>{
     fetch('http://localhost:8080/test')
     .then(response => response.json())
     .then(data => setData(data))
     .catch(error => console.log(error));
   })
+  const handleSignout=()=>{
+    router.back()
+  }
 
-  return(
+  return (
     <div>
-      <nav>
-      <p className="text-3xl font-bold underline text-white">
-    Hello world!
-  </p>
-        {/* make logout button */}
-        <button type="button">
-          logout
-        </button>
-      </nav>
-      <h1>Dashboard</h1>
-      <button type="button" onClick={()=>alert("hello")}>
-        hello
-        </button>
-        {data.map(item => (
-          <div key={item.id}>
-            {item.regionid}
-            {item.name}
-            <button type="button" onClick={()=>alert(item.id)}>
+      <div className="flex min-h-screen bg-gradient-to-tr from-slate-900 to-sky-200">
+        <div className="fade-in text-center pt-20 relative pb-8 sm:mx-auto sm:px-1 sm:max-w-xl">
+          <div className="px-4 pt-4 sm:px-0 ">
+            <nav>
+              <p className="text-3xl font-bold underline text-white">
+                Hello world!
+              </p>
+              {/* make logout button */}
+              <button 
+              onClick={handleSignout}
+              type="button">logout</button>
+            </nav>
+            <h1>Dashboard</h1>
+            <button type="button" onClick={() => alert("hello")}>
               hello
-              </button>
-            </div>
-        )
-        )}
-
+            </button>
+            {data.map((item) => (
+              <div key={item.id}>
+                {item.regionid}
+                {item.name}
+                <button type="button" onClick={() => alert(item.id)}>
+                  hello
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,9 @@ export default function Home() {
   const [formData,setFormdata] = useState({email:"",password:""})
   const router = useRouter()
 
-  const handleSubmit =  (e) => {
-    e.preventDefault();
-    if(e.email === "" || e.password==="" ){
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if(formData.email === "" || formData.password ==="" ){
       alert('please enter your email or password')
     }else{
       router.push('/dashboard')
@@ -18,33 +18,63 @@ export default function Home() {
   }
   
   return (
-    <>
-      <div className='text-3xl text-green-600 p-2'>
-      Hello Geeks!
+    <div className="flex min-h-screen bg-gradient-to-tr from-slate-900 to-blue-900">
+      <div className="fade-in text-center pt-20 relative pb-8 sm:mx-auto sm:px-1 sm:max-w-xl">
+        <div className="px-4 pt-4 sm:px-0 ">
+          <div className="max-w-sm bg-sky-200 rounded-lg border border-gray-200 shadow-md">
+            <form onSubmit={handleSubmit} className="p-10">
+              <label className="text-2xl font-bold ">
+                Email:
+                <input
+                  type="email"
+                  name="email"
+                  className="relative block w-full appearance-none rounded-lg border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm"
+                  placeholder="Enter your email"
+                  value={formData.email}
+                  onChange={(e) =>
+                    setFormdata({ ...formData, email: e.target.value })
+                  }
+                />
+              </label>
+              <br />
+              <label className="text-2xl font-bold ">
+                Password:
+                <input
+                  type="password"
+                  name="password"
+                  className="relative block w-full appearance-none rounded-lg border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm"
+                  placeholder="Enter your password"
+                  value={formData.password}
+                  onChange={(e) =>
+                    setFormdata({ ...formData, password: e.target.value })
+                  }
+                />
+              </label>
+              <br />
+              <button
+                type="submit"
+                className="py-3 px-3 dark:bg-emerald-400/20 dark:text-emerald-200 text-sm font-semibold rounded-md shadow-lg focus:outline-none bg-emerald-500/50 hover:shadow-emerald-400/30 hover:bg-emerald-600"
+              >
+                <span className="flex content-start items-center text-cyan-800 hover:text-emerald-800">
+                  Login
+                  <svg
+                    className="ml-2 -mr-1 w-4 h-4"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                </span>
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email:
-          <input 
-            type="email"
-            name="email" 
-            placeholder="Enter your email"
-            onChange={(e) => setFormdata({...formData,email: e.target.value})}
-          />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input 
-            type="password"
-            name="password"
-            placeholder="Enter your password" 
-            onChange={(e)=> setFormdata({...formData,password:e.target.value})}
-          />
-        </label>
-        <br />
-        <button type="submit">Login</button>
-      </form>
-    </>
-  )
+  );
 }
